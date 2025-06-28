586e513624a6d4a690fc305c3a9cc491cdd5f8fa
elliot <blueallie3346@gmail.com>
2025-06-28 19:33:17 +0530

    added opacity

--- script.js
+++ script.js
@@ -11,28 +11,13 @@ function newGrid(){
         box.classList.add("box")
         row.append(box)
         container.append(row)
+        let op = 0
         box.addEventListener("mouseover",()=>{
-            box.classList.add("hovered")
+            box.style.backgroundColor="black"
+            box.style.opacity= op+0.1
+            op=op+0.1
         })
     }
 }
 }
-for (j = 0; j < 16; j++) {
-    const row = document.createElement("div")
-    row.classList.add("row")
-    for (i = 0; i < 16; i++) {
-        const box = document.createElement("div")
-        box.classList.add("box")
-        row.append(box)
-        container.append(row)
-    }
-}
-
-const box = document.querySelectorAll(".box")
-
-function hov(part) {
-    part.addEventListener("mouseover",()=>{
-        part.classList.add("hovered")
-    })
-}
-box.forEach(hov)
+newGrid()
