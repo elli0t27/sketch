ac4f385436c4f0fd77709052e53d63162fa99c64
elliot <blueallie3346@gmail.com>
2025-06-28 17:29:43 +0530

    basic functioning done

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector(".grid")
+
+function newGrid(){
+    const num = parseInt(prompt("Enter num of grid"))
+    container.innerHTML=""
+    for (j = 0; j < num; j++) {
+    const row = document.createElement("div")
+    for (i = 0; i < num; i++) {
+        const box = document.createElement("div")
+        box.classList.add("box")
+        row.append(box)
+        container.append(row)
+        box.addEventListener("mouseover",()=>{
+            box.classList.add("hovered")
+        })
+    }
+}
+}
+for (j = 0; j < 16; j++) {
+    const row = document.createElement("div")
+    for (i = 0; i < 16; i++) {
+        const box = document.createElement("div")
+        box.classList.add("box")
+        row.append(box)
+        container.append(row)
+    }
+}
+
+const box = document.querySelectorAll(".box")
+
+function hov(part) {
+    part.addEventListener("mouseover",()=>{
+        part.classList.add("hovered")
+    })
+}
+box.forEach(hov)
